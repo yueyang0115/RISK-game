9ef84b89ca82fbc1ee38c22731bc954730e32460
Weihan <wz125@duke.edu>
2020-03-10 14:29:42 -0400

    parse_function

--- shared/src/main/java/edu/duke/ece651/shared/MapFormatter.java
+++ shared/src/main/java/edu/duke/ece651/shared/MapFormatter.java
@@ -1,8 +1,19 @@
 package edu.duke.ece651.shared;
+import java.utils.*;
+import org.json;
 
 public class MapFormatter implements Formatter{
   void compose(){
   }
-  void parse(){
+  HashMap<Integer, ArrayList<Territory>> parse(InputStream MapJson){
+    BufferedReader br = new BufferedReader(new InputStreamReader(MapJson));
+    String str = "";
+    String line = br.readLine();
+    while(line != null){
+      str += line;
+      line = br.readLine();
+    }
+    JsonObject InputMap = new JSONObject(str);
+    
   }
 }
