b749a1a7c65567d0b86325d127c120ace113ab9f
Weihan <wz125@duke.edu>
2020-03-10 13:47:36 -0400

    formatter interface and class

---
+++ shared/src/main/java/edu/duke/ece651/shared/Formatter.java
@@ -0,0 +1,6 @@
+package edu.duke.ece651.shared;
+
+public class Formatter {
+  void compose();
+  void parse();
+}
---
+++ shared/src/main/java/edu/duke/ece651/shared/MapFormatter.java
@@ -0,0 +1,8 @@
+package edu.duke.ece651.shared;
+
+public class MapFormatter implements Formatter{
+  void compose(){
+  }
+  void parse(){
+  }
+}
