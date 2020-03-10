c0bb4a9177b0e3e0895183098b5b5f8cc625ee99
Weihan <wz125@duke.edu>
2020-03-10 17:39:09 -0400

    finish parseMap

--- shared/src/main/java/edu/duke/ece651/shared/Formatter.java
+++ shared/src/main/java/edu/duke/ece651/shared/Formatter.java
@@ -1,6 +1,40 @@
 package edu.duke.ece651.shared;
-
-public class Formatter {
-  void compose();
-  void parse();
+import java.util.*;
+import org.json.*;
+public class Formatter{
+  private int NumPlayers;
+  Formatter(int num){
+    NumPlayers = num;
+  }
+  public void MapCompose(){
+  }
+  public void MapParse(HashMap<Integer, ArrayList<Territory>>Input, String MapJson){
+    JSONObject InputMap = new JSONObject(MapJson);
+    for(int i = 0; i < NumPlayers; i++){
+      JSONArray PlayerTemp = InputMap.getJSONArray("player_" + Integer.toString(i));
+      System.out.println("[DEBUG] Player = " + i);
+      ArrayList<Territory> InnerTerr = new ArrayList<Territory>();
+      for(int j = 0; j < PlayerTemp.length(); j++){
+        Territory Inner = new Territory();
+        JSONObject TerrTemp = PlayerTemp.optJSONObject(j);
+        String owner = TerrTemp.optString("owner");
+        System.out.println("[DEBUG] OwnerName = " + owner);
+        Inner.setOwner(owner);
+        int SoldierNum = TerrTemp.optInt("soldiers");
+        Inner.setSoldiers(SoldierNum);
+        System.out.println("[DEBUG] SoldierNum = " + SoldierNum);
+        JSONArray NeighborArray = TerrTemp.optJSONArray("neighbor");
+        for(int k = 0; k < NeighborArray.length(); k++){
+          JSONObject InnerNeigh = NeighborArray.optJSONObject(k);
+          String NeighName = InnerNeigh.optString("neighbor_" + Integer.toString(k));
+          System.out.println("[DEBUG] NeighborName_" + i + " = " + NeighName);
+          Inner.setNeighbor(NeighName);
+        }
+        String TerritoryName = TerrTemp.optString("territoryName");
+        Inner.setTerritoryName(TerritoryName);
+        InnerTerr.add(Inner);
+       }
+      Input.put(i, InnerTerr);
+    }
+  }
 }
--- shared/src/main/java/edu/duke/ece651/shared/Territory.java
+++ shared/src/main/java/edu/duke/ece651/shared/Territory.java
@@ -4,7 +4,22 @@ import java.util.*;
 
 public class Territory {
   private String owner;
-  private HashMap<Soilder, Integer>  soilders;
+  private int soldiers;
   private ArrayList<String> neighbor;
   private String territoryName;
+  Territory(){
+    neighbor = new ArrayList<String>();
+  }
+  public void setOwner(String Owner){
+    owner = Owner;
+  }
+  public void setSoldiers(int SoldierNum){
+    soldiers = SoldierNum;
+  }
+  public void setNeighbor(String neigh){
+    neighbor.add(neigh);
+  }
+  public void setTerritoryName(String Name){
+    territoryName = Name;
+  }
 }
