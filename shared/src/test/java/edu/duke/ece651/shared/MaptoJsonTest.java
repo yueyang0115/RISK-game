9281a04ffa4bbe18969c83d5e44b7e54f646146b
yy <yueyang0115@outlook.com>
2020-03-11 16:26:09 -0400

    add maptojson.java

---
+++ shared/src/test/java/edu/duke/ece651/shared/MaptoJsonTest.java
@@ -0,0 +1,28 @@
+package edu.duke.ece651.shared;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.*;
+import java.util.HashMap;
+import org.junit.jupiter.api.Test;
+
+public class MaptoJsonTest {
+  @Test
+  public void test_toJSON() {
+    HashMap<Integer, ArrayList<Territory>> myterritoryMap =
+        new HashMap<Integer, ArrayList<Territory>>();
+
+    ArrayList<Territory> myTerritoryList = new ArrayList<Territory>();
+    Territory territory_A = new Territory();
+    territory_A.setNeighbor("B");
+    territory_A.setNeighbor("D");
+    territory_A.setOwner("player_0");
+    territory_A.setSoldiers(3);
+    territory_A.setTerritoryName("A");
+    myTerritoryList.add(territory_A);
+
+    myterritoryMap.put(0, myTerritoryList);
+    MaptoJson mytoJSON = new MaptoJson(myterritoryMap);
+    System.out.println(mytoJSON.getJSON());
+  }
+}
