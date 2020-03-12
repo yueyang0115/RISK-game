127cf31d35db2abdff122615ea5908ea1e38eeaf
Jingyi Xie <jingyi.xie@duke.edu>
2020-03-12 00:11:51 -0400

    finished communicator

--- shared/src/main/java/edu/duke/ece651/shared/Communicator.java
+++ shared/src/main/java/edu/duke/ece651/shared/Communicator.java
@@ -6,9 +6,6 @@ import java.io.*;
 import org.json.*;
 
 public class Communicator {
-  //private Socket socket;
-  //private OutputStreamWriter out;
-  //private BufferedReader in;
 
   public Socket getCommunicator(int port) {
     try {
@@ -24,36 +21,42 @@ public class Communicator {
   public Socket getCommunicator(String ip, int port) {
     try {
       Socket socket = new Socket(ip, port);
+      return socket;
     } catch (IOException e) {
       System.out.println("Failed to crete Communicator!");
     }
+    return null;
+  }
+
+  public void sendString(Socket socket, String str) {
+     try (PrintWriter out = new PrintWriter(socket.getOutputStream(), true)) {
+      out.println(str);
+    } catch (IOException e) {
+      System.out.println("Failed to send String!");
+    }   
   }
   
-  public void send(JSONObject json) {
-    try {
-      this.out = new OutputStreamWriter(this.socket.getOutputStream(), StandardCharsets.UTF_8);
-      this.out.write(json.toString());
+  public void sendJSON(Socket socket, JSONObject json) {
+    try (OutputStreamWriter out = new OutputStreamWriter(socket.getOutputStream(), StandardCharsets.UTF_8)) {
+      out.write(json.toString());
     } catch (IOException e) {
-      System.out.println("Failed to send data!");
+      System.out.println("Failed to send JSONObject!");
     }
   }
   
-  public String receive() {
+  public String receive(Socket socket) {
     String res = "";
-    try {
-      this.in = new BufferedReader(new InputStreamReader(this.socket.getInputStream()));
-      res = this.in.readLine();
+    try (BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()))) {
+      res = in.readLine();
     } catch (IOException e) {
       System.out.println("Failed to receive data!");
     }
     return res;
   }
 
-  public void close() {
+  public void close(Socket socket) {
     try {
-      this.socket.close();
-      this.in.close();
-      this.out.close();
+      socket.close();
     } catch (IOException e) {
       System.out.println("Failed to close socket!");
     }
