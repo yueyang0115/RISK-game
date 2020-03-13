0fb724f8450ebdb98fe23b01441c1259bfdfbed7
jx95 <jx95@duke.edu>
2020-03-13 15:42:20 -0400

    Update player

--- player/src/main/java/edu/duke/ece651/player/Player.java
+++ player/src/main/java/edu/duke/ece651/player/Player.java
@@ -2,7 +2,7 @@ package edu.duke.ece651.player;
 
 import edu.duke.ece651.shared.*;
 import javafx.util.*;
-
+import java.net.*;
 import java.util.*;
 
 public class Player {
@@ -10,11 +10,37 @@ public class Player {
   private Pair<Integer, String> playerInfo;
   private ArrayList<Action> allAction;
   private Displayable displayer;
+  private Communicator communicator;
+  private int playerNum;
   
   public Player() {
     this.territoryMap = new HashMap<>();
-    //this.playerInfo = new Pair<>();
     this.allAction = new ArrayList<>();
+    this.communicator = new Communicator("127.0.0.1", 1234);
+    this.playerNum = 0;
+  }
+
+  public void init() {
+    int id = Integer.parseInt(receiveString());
+    //System.out.println("my id is " + id);
+    String color = new ColorID().getPlayerColor(id);
+    //System.out.println(color);
+    this.playerInfo = new Pair<>(id, color);
+    playerNum = Integer.parseInt(receiveString());
+    //System.out.println(playerNum);
+    receiveMap();
+    display();
+  }
+
+  public String receiveString() {
+    return communicator.receive();
+  }
+
+  public void receiveMap() {
+    String str = communicator.receive();
+    System.out.println(str);
+    MyFormatter myformatter = new MyFormatter(playerNum);
+    myformatter.MapParse(territoryMap, str);
   }
 
   public void addDisplayable(Displayable d) {
@@ -22,11 +48,19 @@ public class Player {
   }
 
   public void display() {
-    //displayer.show();
+    displayer.show(territoryMap, playerInfo);
+  }
+
+  public void close() {
+    communicator.close();
   }
 
   public static void main(String[] args) {
-    System.out.println("hello");
+    Player player = new Player();
+    Displayable d = new Text();
+    player.addDisplayable(d);
+    player.init();
+    player.close();
   }
   
 }
