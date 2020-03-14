b83343728e3d8e3d42e42766fb08c178a934f7c1
jx95 <jx95@duke.edu>
2020-03-13 21:28:57 -0400

    Update test.sh

--- scripts/test.sh
+++ scripts/test.sh
@@ -5,5 +5,5 @@
 scripts/coverage_summary.sh
 ls -l /
 ls -l /coverage-out/
-cp -r build/reports/clover/html/* /coverage-out/ || exit 1
-
+cp -r server/build/reports/clover/html/* /coverage-out/ || exit 1
+cp -r server/build/reports/clover/html/* /coverage-out/ || exit 1
