6cb6dde6656f76d2126de16a44de177b676a9f77
yy <yueyang0115@outlook.com>
2020-03-21 01:17:01 -0400

    remove directory for merge

--- player/src/main/java/edu/duke/ece651/player/Displayable.java
+++
@@ -1,8 +0,0 @@
-package edu.duke.ece651.player;
-
-import java.util.*;
-import edu.duke.ece651.shared.*;
-import javafx.util.*;
-public interface Displayable {
-  public void show(HashMap<Integer, ArrayList<Territory>> CurrentMap, Pair<Integer, String> playerInfo);
-}
