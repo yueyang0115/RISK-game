548c6b0ff5d9c7eb8cbb64d45636f2ac8442eb52
yy <yueyang0115@outlook.com>
2020-03-29 21:23:56 -0400

    modify soldiers in MyFormatter.java

--- shared/src/main/java/edu/duke/ece651/shared/MyFormatter.java
+++ shared/src/main/java/edu/duke/ece651/shared/MyFormatter.java
@@ -17,10 +17,10 @@ public class MyFormatter {
   public void MapParse(HashMap<Integer, ArrayList<Territory>> Input, String MapJson) {
     // receive the json string Map and parse into a territoryMap
     JSONObject InputMap = new JSONObject(MapJson);
-    //search through all player to set their corresponding territory
+    // search through all player to set their corresponding territory
     for (int i = 0; i < NumPlayers; i++) {
       JSONArray PlayerTemp = new JSONArray();
-      
+
       PlayerTemp = InputMap.optJSONArray("player_" + Integer.toString(i));
 
       if (PlayerTemp != null) {
@@ -37,19 +37,22 @@ public class MyFormatter {
         }
       }
     }
-    
   }
 
   public Territory JsonToTerritory(JSONObject TerrTemp) {
-    //parse One JsonObject which is one territory
+    // parse One JsonObject which is one territory
     Territory Inner = new Territory();
     String owner = TerrTemp.optString("owner");
     // System.out.println("[DEBUG] OwnerName = " + owner);
     Inner.setOwner(owner);
-    int SoldierNum = TerrTemp.optInt("soldiers");
-    Inner.setSoldiers(SoldierNum);
+    JSONArray SoldierArray = TerrTemp.optJSONArray("soldiers");
+    for (int i = 0; i < SoldierArray.length(); i++) {
+      JSONObject InnerSoldier = SoldierArray.optJSONObject(i);
+      int num = InnerSoldier.optInt("level_" + Integer.toString(i));
+      Inner.setSoldierLevel(i, num);
+    }
     // System.out.println("[DEBUG] SoldierNum = " + SoldierNum);
-    //neighbor is stored in JsonArray Format
+    // neighbor is stored in JsonArray Format
     JSONArray NeighborArray = TerrTemp.optJSONArray("neighbor");
     for (int k = 0; k < NeighborArray.length(); k++) {
       JSONObject InnerNeigh = NeighborArray.optJSONObject(k);
@@ -64,16 +67,16 @@ public class MyFormatter {
   }
 
   public JSONObject ActionCompose(ArrayList<Action> actionList, String ActionType) {
-    //based on Action type to compose the arraylist action into JsonObject
+    // based on Action type to compose the arraylist action into JsonObject
     ActiontoJson myActiontoJson = new ActiontoJson(actionList, ActionType);
     // System.out.println("The returned Action is: " + myActiontoJson.getJSON());
     return myActiontoJson.getJSON();
   }
 
   public void ActionParse(ArrayList<Action> Input, String ActionJson) {
-    //parse action json string into an arraylist
+    // parse action json string into an arraylist
     JSONObject InputAction = new JSONObject(ActionJson);
-    //get the entire jsonArray
+    // get the entire jsonArray
     String ActionType;
     if (InputAction.has("Move")) {
       ActionType = "Move";
@@ -85,7 +88,7 @@ public class MyFormatter {
   }
 
   private void ParseActionArray(ArrayList<Action> Input, JSONArray ActionArray) {
-    //parse each territory in the jsonarray
+    // parse each territory in the jsonarray
     for (int i = 0; i < ActionArray.length(); i++) {
       Action InnerAction = new Action();
       JSONObject ActionTemp = ActionArray.optJSONObject(i);
@@ -94,8 +97,12 @@ public class MyFormatter {
       String ActionType = ActionTemp.optString("type");
       // System.out.println("Action Type in Parse " + ActionType);
       InnerAction.setType(ActionType);
-      int ActionSoldierNum = ActionTemp.optInt("soldiers");
-      InnerAction.setSoldiers(ActionSoldierNum);
+      JSONArray SoldierArray = ActionTemp.optJSONArray("soldiers");
+      for (int j = 0; j < SoldierArray.length(); j++) {
+        JSONObject InnerSoldier = SoldierArray.optJSONObject(i);
+        int num = InnerSoldier.optInt("level_" + Integer.toString(i));
+        InnerAction.setSoldierLevel(i, num);
+      }
       setSrcDst(InnerAction, ActionTemp, "src");
       setSrcDst(InnerAction, ActionTemp, "dst");
       Input.add(InnerAction);
@@ -113,7 +120,7 @@ public class MyFormatter {
   }
 
   public void AllActionParse(HashMap<Integer, ArrayList<Action>> AllAction, String Input) {
-    //get a jsonstring of all players actions and parse that into a hashmap
+    // get a jsonstring of all players actions and parse that into a hashmap
     // System.out.println("The Received All Action String is (ready to AllActionParse): " + Input);
     JSONObject InputAction = new JSONObject(Input);
     // System.out.println("Input  = " + Input);
@@ -131,7 +138,7 @@ public class MyFormatter {
     }
   }
   public JSONObject AllActionCompose(HashMap<Integer, ArrayList<Action>> AllAction) {
-    //compose the jsonstring based on the hashMap AllAction
+    // compose the jsonstring based on the hashMap AllAction
     ActiontoJson myAllAction = new ActiontoJson(AllAction);
     myAllAction.ComposeAllAction();
     return myAllAction.getAllAction();
