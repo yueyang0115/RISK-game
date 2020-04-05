03053fb45054342f0f6d15d630e6b47d5f39fcf6
yueyang <yueyang0115@outlook.com>
2020-04-05 02:26:56 -0400

    add Lose controller, lead to End.fxml and Watch.fxml

---
+++ player/src/main/java/edu/duke/ece651/player/End.java
@@ -0,0 +1,18 @@
+package edu.duke.ece651.player;
+
+import edu.duke.ece651.shared.*;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.text.Font;
+import javafx.stage.Stage;
+import javafx.util.Pair;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class End {
+
+}
