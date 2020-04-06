3df24b44aeea6722a7020599462e56f764af03f5
wz125-0520 <55336624+wz125-0520@users.noreply.github.com>
2020-04-06 11:31:02 -0400

    Add Player Part tests

--- player/src/main/java/edu/duke/ece651/player/Player.java
+++ player/src/main/java/edu/duke/ece651/player/Player.java
@@ -13,7 +13,6 @@ import javafx.fxml.*;
 
 public class Player extends Application {
   private Stage Window;
-  //private Communicator communicator;
 
   public void showStartView(PlayerHelper player) throws IOException {
     FXMLLoader loaderStart = new FXMLLoader(getClass().getResource("/StartGame.fxml"));
@@ -48,36 +47,6 @@ public class Player extends Application {
     sendString(String.valueOf(PlayerNum), communicator);
   }
 
-
-
-/*
-  public void init(Scanner scanner) {
-    
-    int id = this.playerInfo.getKey();
-    //the first player input the total number of players
-    if (id == 0) {
-      System.out.println(
-          "===You're the first player, please enter the number of all players ([2:5])===");
-      int playerNum = scanner.nextInt();
-      while (playerNum < 2 || playerNum > 5) {
-        System.out.println("========Invalid playerNumber, try again ([2:5])========");
-        playerNum = scanner.nextInt();
-      }
-      sendString(String.valueOf(playerNum));
-      //send it to server
-    }
-    // System.out.println("[DEBUG] my id is " + id);
-    String color = new ColorID().getPlayerColor(id);
-    this.playerInfo = new Pair<>(id, color);
-    playerNum = Integer.parseInt(receiveString());
-  }
-
-
-
-
-
-
-*/
   public void sendString(String str, Communicator communicator) {
     communicator.sendString(str);
   }
--- player/src/test/java/edu/duke/ece651/player/OperateActionTest.java
+++ player/src/test/java/edu/duke/ece651/player/OperateActionTest.java
@@ -11,7 +11,7 @@ import org.junit.jupiter.api.Test;
 public class OperateActionTest {
   @Test
   public void test_OperateAction() throws FileNotFoundException{
-    /*String Input = "{'player_0':[ {'owner':'player_0', 'soldiers':3, 'neighbor':[{'neighbor_0':'B'},{'neighbor_1':'D'}], 'territoryName':'A'}, {'owner':'player_0', 'soldiers':3, 'neighbor':[{'neighbor_0':'A'},{'neighbor_1':'C'},{'neighbor_2':'D'},{'neighbor_3':'E'},{'neighbor_4':'F'}], 'territoryName':'B'}, {'owner':'player_0', 'soldiers':3, 'neighbor':[{'neighbor_0':'B'},{'neighbor_1':'F'}], 'territoryName':'C'}, {'owner':'player_0', 'soldiers':3, 'neighbor':[{'neighbor_0':'A'},{'neighbor_1':'B'},{'neighbor_2':'E'},{'neighbor_3':'G'}], 'territoryName':'D'}],'player_1':[ {'owner':'player_1', 'soldiers':3, 'neighbor':[{'neighbor_0':'B'},{'neighbor_1':'D'},{'neighbor_2':'F'},{'neighbor_3':'G'},{'neighbor_4':'H'}], 'territoryName':'E'}, {'owner':'player_1', 'soldiers':3, 'neighbor':[{'neighbor_0':'B'},{'neighbor_1':'C'},{'neighbor_2':'E'},{'neighbor_3':'H'}], 'territoryName':'F'}, {'owner':'player_1', 'soldiers':3, 'neighbor':[{'neighbor_0':'D'},{'neighbor_1':'E'}], 'territoryName':'G'}, {'owner':'player_1', 'soldiers':3, 'neighbor':[{'neighbor_0':'E'},{'neighbor_1':'F'}], 'territoryName':'H'}]}";   
+    String Input = "{'player_0':[ {'owner':'player_0', 'soldiers':[{'level_0':'3'},{'level_1':'0'},{'level_2':'0'},{'level_3':'0'},{'level_4':'0'},{'level_5':'0'},{'level_6':'0'}], 'neighbor':[{'neighbor_0':'B'},{'neighbor_1':'C'},{'neighbor_2':'D'}], 'territoryName':'A'},{'owner':'player_0', 'soldiers':[{'level_0':'3'},{'level_1':'0'},{'level_2':'0'},{'level_3':'0'},{'level_4':'0'},{'level_5':'0'},{'level_6':'0'}], 'neighbor':[{'neighbor_0':'A'},{'neighbor_1':'C'},{'neighbor_2':'L'}], 'territoryName':'B'},{'owner':'player_0', 'soldiers':[{'level_0':'3'},{'level_1':'0'},{'level_2':'0'},{'level_3':'0'},{'level_4':'0'},{'level_5':'0'},{'level_6':'0'}], 'neighbor':[{'neighbor_0':'A'},{'neighbor_1':'B'},{'neighbor_2':'L'},{'neighbor_3':'J'},{'neighbor_4':'F'},{'neighbor_5':'D'}], 'territoryName':'C'},{'owner':'player_0', 'soldiers':[{'level_0':'3'},{'level_1':'0'},{'level_2':'0'},{'level_3':'0'},{'level_4':'0'},{'level_5':'0'},{'level_6':'0'}], 'neighbor':[{'neighbor_0':'A'},{'neighbor_1':'C'},{'neighbor_2':'F'}, {'neighbor_3':'E'}], 'territoryName':'D'},{'owner':'player_0', 'soldiers':[{'level_0':'3'},{'level_1':'0'},{'level_2':'0'},{'level_3':'0'},{'level_4':'0'},{'level_5':'0'},{'level_6':'0'}], 'neighbor':[{'neighbor_0':'D'},{'neighbor_1':'F'}, {'neighbor_2':'G'}], 'territoryName':'E'},{'owner':'player_0', 'soldiers':[{'level_0':'3'},{'level_1':'0'},{'level_2':'0'},{'level_3':'0'},{'level_4':'0'},{'level_5':'0'},{'level_6':'0'}], 'neighbor':[{'neighbor_0':'D'},{'neighbor_1':'C'},{'neighbor_2':'J'},{'neighbor_3':'I'},{'neighbor_4':'G'},{'neighbor_5':'E'}], 'territoryName':'F'}], 'player_1':[ {'owner':'player_1', 'soldiers':[{'level_0':'3'},{'level_1':'0'},{'level_2':'0'},{'level_3':'0'},{'level_4':'0'},{'level_5':'0'},{'level_6':'0'}], 'neighbor':[{'neighbor_0':'E'},{'neighbor_1':'F'},{'neighbor_2':'I'},{'neighbor_3':'H'}], 'territoryName':'G'}, {'owner':'player_1', 'soldiers':[{'level_0':'3'},{'level_1':'0'},{'level_2':'0'},{'level_3':'0'},{'level_4':'0'},{'level_5':'0'},{'level_6':'0'}], 'neighbor':[{'neighbor_0':'G'},{'neighbor_1':'I'}], 'territoryName':'H'}, {'owner':'player_1', 'soldiers':[{'level_0':'3'},{'level_1':'0'},{'level_2':'0'},{'level_3':'0'},{'level_4':'0'},{'level_5':'0'},{'level_6':'0'}], 'neighbor':[{'neighbor_0':'F'},{'neighbor_1':'J'},{'neighbor_2':'K'},{'neighbor_3':'H'},{'neighbor_4':'G'}], 'territoryName':'I'}, {'owner':'player_1', 'soldiers':[{'level_0':'3'},{'level_1':'0'},{'level_2':'0'},{'level_3':'0'},{'level_4':'0'},{'level_5':'0'},{'level_6':'0'}], 'neighbor':[{'neighbor_0':'C'},{'neighbor_1':'L'},{'neighbor_2':'K'},{'neighbor_3':'I'},{'neighbor_4':'F'}], 'territoryName':'J'},{'owner':'player_1', 'soldiers':[{'level_0':'3'},{'level_1':'0'},{'level_2':'0'},{'level_3':'0'},{'level_4':'0'},{'level_5':'0'},{'level_6':'0'}], 'neighbor':[{'neighbor_0':'I'},{'neighbor_1':'J'},{'neighbor_2':'L'}], 'territoryName':'K'},{'owner':'player_1', 'soldiers':[{'level_0':'3'},{'level_1':'0'},{'level_2':'0'},{'level_3':'0'},{'level_4':'0'},{'level_5':'0'},{'level_6':'0'}], 'neighbor':[{'neighbor_0':'B'},{'neighbor_1':'C'},{'neighbor_2':'J'},{'neighbor_3':'K'}], 'territoryName':'L'}] }";
     MyFormatter Map = new MyFormatter(2);
     HashMap<Integer, ArrayList<Territory>> TerritoryMap = new HashMap<>();
     Map.MapParse(TerritoryMap, Input);
@@ -20,9 +20,6 @@ public class OperateActionTest {
     
     System.setIn(new FileInputStream("src/test/resources/TestReadAction1.txt"));
     readTest1.readAction();
-
-    
-
     System.out.println("[DEBUG] MoveAction:");
     for(Action Temp: readTest1.getMoveActions()){
       System.out.println("[DEBUG] OwnerName = " + Temp.getOwner());
@@ -42,7 +39,6 @@ public class OperateActionTest {
       System.out.println("Number of Soliders = " + Temp.getSoldierLevel(0));
       System.out.print("\n");
     }
-    */
   }
 
 }
