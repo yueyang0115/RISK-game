d2df2d114f38f6c7e9e2b8d913dd3f3fc2708d32
jx95 <jx95@duke.edu>
2020-04-11 23:41:30 -0400

    start alliance

--- player/src/main/java/edu/duke/ece651/player/Alliance.java
+++ player/src/main/java/edu/duke/ece651/player/Alliance.java
@@ -1,5 +1,4 @@
 package edu.duke.ece651.player;
-
 import edu.duke.ece651.shared.*;
 import javafx.animation.PauseTransition;
 import javafx.fxml.FXML;
@@ -15,11 +14,8 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 
-public class Map{
+public class Alliance {
     //------------- Evolution 2 --------------//
-    @FXML private Button UpgradeBtn;
-    @FXML private Button MoveBtn;
-    @FXML private Button AttackBtn;
     @FXML private Button DoneBtn;
 
     @FXML private Button ButtonA;
@@ -39,8 +35,6 @@ public class Map{
     @FXML private Label Prompt;
 
     private Stage Window;
-    private Boolean firstTime;
-
     private PlayerHelper CurrPlayer;
     private HashMap<String, Button> ButtonMap;
     private HashMap<Integer, ArrayList<Territory>> TerrMap;
@@ -59,11 +53,10 @@ public class Map{
         ButtonMap.put("K", ButtonK);
         ButtonMap.put("L", ButtonL);
     }
-    public Map(PlayerHelper player, Stage Window, Boolean first){
+    public Alliance(PlayerHelper player, Stage Window){
         this.Window = Window;
         this.CurrPlayer = player;
         this.TerrMap = player.getTerritoryMap();
-        this.firstTime = first;
     }
 
     public void initialize(){
@@ -71,20 +64,8 @@ public class Map{
         new Graph().showMap(this.CurrPlayer.getTerritoryMap(), this.CurrPlayer.getPlayerInfo(), this.ButtonMap);
         ColorID PlayerColor = new ColorID();
         String PlayerName = PlayerColor.getPlayerColor(this.CurrPlayer.getPlayerInfo().getKey());
-        this.Prompt.setText("You are " + PlayerName + " Player, please choose action");
+        this.Prompt.setText("You are " + PlayerName + " Player, please choose which player to form alliance");
         this.Prompt.setFont(new Font("Arial", 28));
-        if (firstTime) {
-            PauseTransition delay = new PauseTransition(Duration.seconds(1));
-            delay.setOnFinished(event -> {
-                try {
-                    showChat();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                    System.out.println("Exception "+ e);
-                }
-            });
-            delay.play();
-        }
     }
 
     //if the player click the button, show the detail of each territory in the right side label
@@ -94,6 +75,7 @@ public class Map{
         Territory CurrentClicked =  Show.FindTerritory(this.TerrMap, "A");
         Show.ShowLabel(CurrentClicked, this.Detail);
     }
+
 
     @FXML
     public void BtnB(){
@@ -161,101 +143,9 @@ public class Map{
         Territory CurrentClicked =  Show.FindTerritory(this.TerrMap, "L");
         Show.ShowLabel(CurrentClicked, this.Detail);
     }
-
-    //-----------------------------------------------//
-    //these three buttons are related to actions
-    //and if click on each of them we jump into different page to finish each actions's input information
     @FXML
-    public void Upgrading() throws IOException {
-        System.out.println("Click on Upgrade");
-        FXMLLoader loaderStart = new FXMLLoader(getClass().getResource("/UpgradeChoose.fxml"));
-        loaderStart.setControllerFactory(c->{
-            return new UpgradeChoose(this.CurrPlayer, Window);
-        });
-        Scene scene = new Scene(loaderStart.load());
-        this.Window.setScene(scene);
-        this.Window.show();
+    public void ChooseDone(){
+        System.out.println("Click on Done");
+
     }
-
-    @FXML
-    public void ChooseMove() throws IOException {
-        System.out.println("Click on Move");
-        FXMLLoader loaderStart = new FXMLLoader(getClass().getResource("/Move_attack.fxml"));
-        loaderStart.setControllerFactory(c->{
-            return new MoveOrAttack(this.CurrPlayer, "Move", this.Window);
-        });
-        Scene scene = new Scene(loaderStart.load());
-        this.Window.setScene(scene);
-        this.Window.show();
-    }
-
-    @FXML
-    public void ChooseAlliance() throws IOException {
-        System.out.println("Click on Alliance");
-        FXMLLoader loaderStart = new FXMLLoader(getClass().getResource("/Alliance.fxml"));
-        loaderStart.setControllerFactory(c->{
-            return new Alliance(this.CurrPlayer, this.Window);
-        });
-        Scene scene = new Scene(loaderStart.load());
-        this.Window.setScene(scene);
-        this.Window.show();
-    }
-
-    @FXML
-    public void ChooseAttack() throws IOException {
-        System.out.println("Click on Attack");
-        FXMLLoader loaderStart = new FXMLLoader(getClass().getResource("/Move_attack.fxml"));
-        loaderStart.setControllerFactory(c->{
-            return new MoveOrAttack(this.CurrPlayer, "Attack", this.Window);
-        });
-        Scene scene = new Scene(loaderStart.load());
-        this.Window.setScene(scene);
-        this.Window.show();
-    }
-
-    @FXML
-    public void ChooseDone() throws IOException {
-        //if it choose done, receive this turn's result information
-        System.out.println("Click on Done in Map");
-        ShowView show = new ShowView();
-        this.CurrPlayer.SendAction();
-        String Validation = this.CurrPlayer.ReceiveActionRes();
-        System.out.println("Validation " + Validation);
-        this.CurrPlayer.ReceiveAllAction();
-
-        this.CurrPlayer.AddTechResource(this.CurrPlayer.getTerritoryMap(),this.CurrPlayer.getPlayerInfo());
-        //the answer could be map or lose game and game end
-        String Answer = this.CurrPlayer.ReceiveFromServer();
-        //check whether the received string is game end or lose game or normal map
-        //display different page with different received string content
-        if(Answer.contains("Game End!")){
-            System.out.println("Received Game End");
-            new ShowView().ShowEndVIew(Answer, this.CurrPlayer, this.Window);
-        }
-        else if(Answer.contains("Lose Game")){
-            System.out.println("Received Lose Game");
-            this.CurrPlayer.setAsk(true);
-            this.CurrPlayer.setLose(true);
-            new ShowView().ShowLoseView(Validation, this.CurrPlayer, this.Window);
-        }
-        else {
-            System.out.println("Normal Received Map");
-            this.CurrPlayer.ContinueReceive(Answer);
-            new ShowView().ShowDoneView(Validation, this.CurrPlayer, this.Window);
-        }
-    }
-
-    public void showChat() throws IOException {
-        ColorID cid = new ColorID();
-        FXMLLoader loaderStart = new FXMLLoader(getClass().getResource("/ChatRoom.fxml"));
-        Stage newWindow = new Stage();
-        loaderStart.setControllerFactory(c->{
-            return new ChatRoom(cid.getPlayerColor(CurrPlayer.getID()), newWindow);
-        });
-        Scene scene = new Scene(loaderStart.load());
-        newWindow.setScene(scene);
-        newWindow.show();
-    }
-
-
 }
