9a11d35f1325fe06954048a995b291243b224fed
wz125-0520 <55336624+wz125-0520@users.noreply.github.com>
2020-04-17 19:48:01 -0400

    Update ChatRoom

--- shared/src/main/java/edu/duke/ece651/shared/ColorID.java
+++ shared/src/main/java/edu/duke/ece651/shared/ColorID.java
@@ -3,6 +3,7 @@ package edu.duke.ece651.shared;
 import java.util.*;
 public class ColorID {
   HashMap<Integer, String> Colorid;
+  HashMap<String, Integer> IDColor;
   public ColorID(){
     Colorid = new HashMap<>();
     Colorid.put(0, "Green");
@@ -10,10 +11,19 @@ public class ColorID {
     Colorid.put(2, "Red");
     Colorid.put(3, "Yellow");
     Colorid.put(4, "White");
+
+    IDColor = new HashMap<>();
+    IDColor.put("Green", 0);
+    IDColor.put("Blue", 1);
+    IDColor.put("Red", 2);
+    IDColor.put("Yellow", 3);
+    IDColor.put("White", 4);
   }
 
   public String getPlayerColor(int ID){
     return Colorid.get(ID);
   }
-
+  public int getPlayerID(String color){
+    return this.IDColor.get(color);
+  }
 }
