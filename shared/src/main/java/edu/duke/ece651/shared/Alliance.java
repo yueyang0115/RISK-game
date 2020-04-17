bccc794d7b149d31f9adaab8996fa49713dab30c
jx95 <jx95@duke.edu>
2020-04-17 11:35:01 -0400

    Add Alliance class, start formatter

---
+++ shared/src/main/java/edu/duke/ece651/shared/Alliance.java
@@ -0,0 +1,21 @@
+package edu.duke.ece651.shared;
+
+public class Alliance {
+    private String owner;
+    private String ally;
+
+    public Alliance() {
+    }
+    public void setOwner(String Owner) {
+        owner = Owner;
+    }
+    public void setAlly(String Ally){
+        ally = Ally;
+    }
+    public String getOwner() {
+        return owner;
+    }
+    public String getAlly(){
+        return ally;
+    }
+}
